04311ef2256427de9f58d53ddd6a18b0a73b7bb7
root <root@h2891400.stratoserver.net>
2020-08-26 11:49:24 +0200

    automatic shell commit

---
+++ run_scraper_commit_log.sh
@@ -0,0 +1,17 @@
+echo "running scraper...";
+python3 /home/simon/Git/metadata_scraper/raw_code.py;
+sleep 5;
+echo "scraping and writing to csvcomplete";
+echo "commiting to GitHub and writing to log file"
+echo "Run and push to repo successful" >> /home/simon/Git/metadata_scraper/log_file.txt;
+date >>/home/simon/Git/metadata_scraper/log_file.txt;
+echo "_______________________________" >> /home/simon/Git/metadata_scraper/log_file.txt;
+git status;
+sleep 2;
+git add *;
+slep 2;
+git commit -m "automatic shell commit";
+sleep 2;
+git push origin master;
+sleep 5;
+echo "Run finished and pushed to Repo";
