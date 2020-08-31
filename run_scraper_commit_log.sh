586d7bf7a9a374d4f30ea428c15a871915bee21d
root <root@h2891400.stratoserver.net>
2020-08-31 10:26:54 +0200

    automatic shell commit

--- run_scraper_commit_log.sh
+++ run_scraper_commit_log.sh
@@ -6,12 +6,13 @@ echo "commiting to GitHub and writing to log file"
 echo "Run and push to repo successful" >> /home/simon/Git/metadata_scraper/log_file.txt;
 date >>/home/simon/Git/metadata_scraper/log_file.txt;
 echo "_______________________________" >> /home/simon/Git/metadata_scraper/log_file.txt;
-git status;
+sleep 10;
+git status >> /home/simon/Git/metadata_scraper/log_file.txt;
 sleep 2;
-git add *;
+git add * >> /home/simon/Git/metadata_scraper/log_file.txt;
 sleep 2;
 git commit -m "automatic shell commit";
 sleep 2;
-git push origin master;
-sleep 5;
+git push origin master >> /home/simon/Git/metadata_scraper/log_file.txt;
+sleep 2;
 echo "Run finished and pushed to Repo";
