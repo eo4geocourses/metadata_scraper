27673d94b3677b1061999510bf3edc010f754871
simon-donike-server <simon.donike@sbg.ac.at>
2020-09-02 15:40:55 +0200

    automatic shell commit

--- run_scraper_commit_log.sh
+++ run_scraper_commit_log.sh
@@ -2,9 +2,8 @@ cd /home/simon/Git/metadata_scraper/;
 echo "running scraper...";
 python3 /home/simon/Git/metadata_scraper/raw_code.py;
 sleep 5;
-echo "scraping and writing to csvcomplete";
-echo "commiting to GitHub and writ ing to log file"
-echo "Run and push to repo successful" >> /home/simon/Git/metadata_scraper/log_file.txt;
+echo "scraping and writing to csv complete";
+echo "commiting to GitHub and writing to log file" >> /home/simon/Git/metadata_scraper/log_file.txt;
 date >>/home/simon/Git/metadata_scraper/log_file.txt;
 echo "_______________________________" >> /home/simon/Git/metadata_scraper/log_file.txt;
 sleep 10;
