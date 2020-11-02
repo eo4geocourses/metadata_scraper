9a76a5fab23b646768027ea85d1e7111fa566bdf
simon-donike <simon.donike@sbg.ac.at>
2020-11-02 19:52:02 +0100

    Update run_scraper_commit_log.sh

--- run_scraper_commit_log.sh
+++ run_scraper_commit_log.sh
@@ -1,7 +1,7 @@
 
 echo '/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\' >> /home/simon/Git/metadata_scraper/log_file.txt;
-echo '\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/' >> /home/simon/Git/metadata_scraper/log_file.txt;
 date >>/home/simon/Git/metadata_scraper/log_file.txt;
+echo '\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/' >> /home/simon/Git/metadata_scraper/log_file.txt;
 cd /home/simon/Git/metadata_scraper/;
 echo "running scraper...";
 python3 /home/simon/Git/metadata_scraper/raw_code.py;
